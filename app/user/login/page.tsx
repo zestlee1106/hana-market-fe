00064b5a0a0f41224327cdff742ad54478f0e4d2
이영우 <zestlee1106@gmail.com>
2024-01-13 19:39:05 +0900

    [feat] 로그인 페이지 추가

--- app/user/login/page.tsx
+++ app/user/login/page.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="card w-96 bg-neutral text-neutral-content">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">로그인 하세요!</h2>
+          <input
+            type="text"
+            placeholder="아이디"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <input
+            type="password"
+            placeholder="비밀번호"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">로그인</button>
+            <button className="btn btn-outline btn-secondary">회원가입</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
